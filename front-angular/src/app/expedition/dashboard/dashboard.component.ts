cc563d3372e3ab40b24c447dce0c99523e5f9469
Daniel Carvalho <danielcarvalho726@gmail.com>
2023-11-05 22:57:43 -0300

    Export Function

--- front-angular/src/app/expedition/dashboard/dashboard.component.ts
+++ front-angular/src/app/expedition/dashboard/dashboard.component.ts
@@ -13,6 +13,7 @@ export class DashboardComponent implements OnInit {
     expeditions: ExpeditionList[] = [];
     todayExpeditions: ExpeditionList[] = [];
     pastExpeditions: ExpeditionList[] = [];
+    selectedExpeditionsIndex: number[] = [];
 
     constructor(
         private expeditionsService: ExpeditionService,
@@ -105,5 +106,71 @@ export class DashboardComponent implements OnInit {
 
     finishExpedition(id: String): void {
         this.router.navigate(['/expedition/end/', id]);
+    }
+
+    // --------------------------------- EXPORT ---------------------------------
+    selectAll: boolean = false;
+    selectAllExpeditions(): void {
+        this.selectedExpeditionsIndex = this.pastExpeditions.map((_, index) => index);
+    }
+
+    deselectAllExpeditions(): void {
+        this.selectedExpeditionsIndex = [];
+    }
+
+    toggleSelectAll(): void {
+        this.selectAll = !this.selectAll;
+        if (this.selectAll) {
+            this.selectAllExpeditions();
+        } else {
+            this.deselectAllExpeditions();
+        }
+    }
+
+    toggleSelection(index: number): void {
+        const idx = this.selectedExpeditionsIndex.indexOf(index);
+        if (idx === -1) {
+            this.selectedExpeditionsIndex.push(index);
+        } else {
+            this.selectedExpeditionsIndex.splice(idx, 1);
+        }
+        this.selectAll = this.selectedExpeditionsIndex.length === this.pastExpeditions.length;
+    }
+
+    isExpeditionSelected(index: number): boolean {
+        return this.selectAll || this.selectedExpeditionsIndex.includes(index);
+    }
+
+    convertToCSV(data: any[]): string {
+        const csv = data.map(row => Object.values(row).join(';'));
+        csv.unshift(Object.keys(data[0]).join(';'));  // Adiciona os cabeçalhos
+        return csv.join('\n');
+    }
+
+    downloadCSV(data: string, filename: string): void {
+        const blob = new Blob([data], { type: 'text/csv' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+
+    exportSelectedExpeditions(): void {
+        // Recuperar expedições selecionadas com base nos índices armazenados
+        const selectedExpeditions = this.selectedExpeditionsIndex.map(index => this.pastExpeditions[index]);
+
+        // Verificar se há alguma expedição selecionada
+        if (selectedExpeditions.length === 0) {
+            console.log('Nenhuma expedição foi selecionada para exportação.');
+            return;
+        }
+
+        // Converte as expedições selecionadas para formato CSV
+        const csvData = this.convertToCSV(selectedExpeditions);
+
+        // Inicia o download do CSV
+        this.downloadCSV(csvData, 'expeditions.csv');
     }
 }
